b9cc248ad194be05ead4e9e253db5e93bfe0547c
malik1243 <154369335+basit1215@users.noreply.github.com>
2024-10-21 14:22:56 +0500

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,90 @@
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, StyleSheet, TextInput, TouchableOpacity, FlatList } from 'react-native'
+import React, { useState } from 'react'
 
 const index = () => {
-  return (
-   <SafeAreaView>
 
-   </SafeAreaView>
-  )
+    const [input, setInput] = useState('')
+    const [todoAdd, setTodoAdd] = useState<string[]>([])
+
+    const addTodo = () => {
+        console.log(input);
+        todoAdd.push(input)
+        setTodoAdd([...todoAdd])
+        setInput('')
+        console.log(todoAdd);
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+
+            <Text style={styles.mainHeading}>Todo App</Text>
+
+            <TextInput
+                style={styles.input}
+                onChangeText={setInput}
+                value={input}
+                placeholder='Enter Todo...'
+            />
+
+            <TouchableOpacity style={styles.button} onPress={addTodo}>
+                <Text>Press Here</Text>
+            </TouchableOpacity>
+
+            {todoAdd.length > 0 ? (<FlatList
+                data={todoAdd}
+                renderItem={({ item }) => (
+                    <View>
+                        <Text>{item}</Text>
+                        <TouchableOpacity style={styles.button} >
+                            <Text>Edit</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={styles.button} >
+                            <Text>Delete</Text>
+                        </TouchableOpacity>
+                    </View >
+                )}
+                keyExtractor={(index) => index.toString()}
+
+            />) : (<Text>No Todo found!</Text>)}
+
+        </SafeAreaView>
+    )
 }
 
-const style = StyleSheet.create({
-    
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    mainHeading: {
+        textAlign: 'center',
+        paddingVertical: 10,
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: 'orange',
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+    button: {
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        padding: 10,
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 32,
+    },
 })
 
 export default index
