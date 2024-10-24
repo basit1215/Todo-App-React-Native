4dc9cc358165dc8f69d2b31c209e312ff6345921
Abdul Basit <154369335+basit1215@users.noreply.github.com>
2024-10-25 03:39:30 +0500

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,3 @@
-// import { Stack } from "expo-router";
-
-// export default function RootLayout() {
-//   return (
-//     <Stack>
-//       <Stack.Screen name="index"  />
-//     </Stack>
-//   );
-// }
-
-
-
-
-
-
-
-
-
-
-
-
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
@@ -29,10 +8,7 @@ export default function RootLayout() {
         options={{
           title: 'Home',
           headerShown: false
-          // headerStyle: {
-          //   backgroundColor: '#c3c3c3'
-          // }
-        }} // Close this properly here
+        }} 
       />
     </Stack>
   );
